926504e721e3b2acd02ad5cfa8128060fcc082ea
Alex Chuchunkov <scarywound@gmail.com>
2017-05-03 00:52:16 +0300

    add domains to test_data

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 import os
+import operator
+
 from flask import Flask, request
 from flask_cors import CORS
 from flask_jsonpify import jsonify
-import random
+
 from test_data import data
+
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/search")
 def search():
-    return jsonify(random.sample(data, random.randint(1, 50)))
+    prefixes = request.args.getlist('query')
+
+    lst = []
+    for (name, domain) in data:
+        if len(prefixes) == 0 or any((domain.startswith(pr) for pr in prefixes)):
+            lst.append(name)
+
+    return jsonify(lst)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
